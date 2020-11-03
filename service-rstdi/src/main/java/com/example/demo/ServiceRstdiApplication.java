278d7c5c293f63825e0e2ab8068713dbb74578a2
liucheng <958392641@qq.com>
2020-11-03 20:20:03 +0800

    first commit

---
+++ service-rstdi/src/main/java/com/example/demo/ServiceRstdiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRstdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRstdiApplication.class, args);
+    }
+
+}
---
+++ service-system/src/main/java/com/example/demo/ServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSystemApplication.class, args);
+    }
+
+}
