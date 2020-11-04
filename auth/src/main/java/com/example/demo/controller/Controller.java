dfa1e2495a7a023c1384fd602296dcf939fe2391
liucheng <958392641@qq.com>
2020-11-04 14:42:56 +0800

    微服务搭建，pom修改

--- auth/src/main/java/com/example/demo/controller/Controller.java
+++ auth/src/main/java/com/example/demo/controller/Controller.java
@@ -1,7 +1,6 @@
-package com.example.demo.Controller;
+package com.example.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@RestController
-@RequestMapping("test1")
 @Slf4j
-public class Test1 {
+@RestController
+@RequestMapping("controller")
+public class Controller {
 
-    @GetMapping("funtion1")
+    @RequestMapping("funtion1")
     public String funtion1(HttpServletRequest req) {
         log.info(req.getRemoteAddr() + "在" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "访问了本机");
-        return "欢迎进入刘呈的个人网站";
+        return "欢迎进入刘呈的个人网站，微服务名为auth";
     }
 }
